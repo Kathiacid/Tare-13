b370c0b8f7ab213c1a18a8c24c84d9e99ef9404f
kat <kathiacid98@gmail.com>
2025-12-28 20:06:57 -0300

    final

--- app.js
+++ app.js
@@ -30,3 +30,47 @@ if (abrir && cerrar) {
         body.classList.remove("no-scroll");
     });
 }
+
+
+let currentSectionIndex = 0;
+const steps = document.querySelectorAll('.step');
+const container = document.getElementById('main-container');
+let isScrolling = false; 
+
+function goToSection(index) {
+
+    if (index < 0 || index >= steps.length) return;
+    
+    currentSectionIndex = index;
+
+    container.style.transform = `translateY(-${currentSectionIndex * 100}vh)`;
+    
+    steps.forEach(step => step.classList.remove('active'));
+    steps[currentSectionIndex].classList.add('active');
+    
+
+    if (index > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+
+
+    isScrolling = true;
+    setTimeout(() => {
+        isScrolling = false;
+    }, 1000); 
+}
+
+
+window.addEventListener('wheel', (e) => {
+    if (isScrolling) return; 
+
+    if (e.deltaY > 0) {
+
+        goToSection(currentSectionIndex + 1);
+    } else {
+        goToSection(currentSectionIndex - 1);
+    }
+});
+
